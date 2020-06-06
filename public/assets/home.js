6fb07839b9ba1d6cd22c63a600cbd3a34c0036bf
hishamss <hishamss90@gmail.com>
2020-06-05 23:11:59 -0700

    created bg img and article cards

--- public/assets/home.js
+++ public/assets/home.js
@@ -1,5 +1,24 @@
 $(document).ready(() => {
   $.get("/all", (response) => {
     console.log(response);
+    for (row of response) {
+      $(".articles").append(
+        `<div class="col mt-3 mb-3 rounded">
+        <div class="card aritcleCard">
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-md-9">
+                        <h5 class=" card-title"><a href="${row.link}" target="_blank">${row.headline}</a>
+                        </h5>
+                        <p class="card-text">${row.description}</p>
+                    </div>
+                    <div class="col-md-3"><img class="img-fluid aritcleImg" src="${row.img}"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="w-100"></div>`
+      );
+    }
   });
 });
