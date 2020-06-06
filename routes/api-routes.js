a3771dee3a568a754bdc789c4f85acc77db4ed7c
hishamss <hishamss90@gmail.com>
2020-06-05 21:12:44 -0700

    implemented the scrape route

---
+++ routes/api-routes.js
@@ -0,0 +1,74 @@
+var cheerio = require("cheerio");
+var axios = require("axios");
+var mongoose = require("mongoose");
+var Article = require("../models/articles");
+mongoose.connect("mongodb://localhost/articlesdb", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
+module.exports = (app) => {
+  app.get("/scrape", (req, res) => {
+    axios
+      .get("https://www.washingtonpost.com/politics/?nid=top_nav_politics")
+      .then((response) => {
+        var $ = cheerio.load(response.data);
+        var results = [];
+        $("div.story-list-story").each((i, element) => {
+          var headline = $(element)
+            .find(".story-body")
+            .children(".story-headline")
+            .find("h2")
+            .children("a")
+            .text();
+          var link = $(element)
+            .find(".story-body")
+            .children(".story-headline")
+            .find("h2")
+            .children("a")
+            .attr("href");
+          var description = $(element)
+            .find(".story-body")
+            .children(".story-description")
+            .children("p")
+            .text();
+          var img = $(element)
+            .find(".story-image")
+            .children("a")
+            .children("img")
+            .attr("src");
+          if (
+            headline !== "" &&
+            description !== "" &&
+            link !== "" &&
+            img !== ""
+          ) {
+            var story = {
+              headline: headline,
+              link: link,
+              description: description,
+              img: img,
+            };
+            results.push(story);
+          }
+        });
+        var oldNumber, newNumber;
+        Article.countDocuments({}, (err, count) => {
+          oldNumber = count;
+          Article.create(results)
+            .then(() => {
+              Article.countDocuments({}, (err, count) => {
+                newNumber = count;
+                res.send(`${newNumber - oldNumber} articles added!`);
+              });
+            })
+            .catch(() => {
+              Article.countDocuments({}, (err, count) => {
+                newNumber = count;
+                res.send(`${newNumber - oldNumber} articles added`);
+              });
+            });
+        });
+      });
+  });
+};
--- server.js
+++ server.js
@@ -1,7 +1,5 @@
-var cheerio = require("cheerio");
-var axios = require("axios");
 var express = require("express");
-var mongoose = require("mongoose");
+
 var exphbs = require("express-handlebars");
 var PORT = process.env.PORT || 3000;
 var app = express();
@@ -11,75 +9,9 @@ app.use(express.json());
 app.use(express.static("public"));
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
-var Article = require("./models/articles");
-mongoose.connect("mongodb://localhost/articlesdb", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-});
-app.get("/", (req, res) => {
-  axios
-    .get("https://www.washingtonpost.com/politics/?nid=top_nav_politics")
-    .then((response) => {
-      var $ = cheerio.load(response.data);
-      var results = [];
-      $("div.story-list-story").each((i, element) => {
-        var headline = $(element)
-          .find(".story-body")
-          .children(".story-headline")
-          .find("h2")
-          .children("a")
-          .text();
-        var link = $(element)
-          .find(".story-body")
-          .children(".story-headline")
-          .find("h2")
-          .children("a")
-          .attr("href");
-        var description = $(element)
-          .find(".story-body")
-          .children(".story-description")
-          .children("p")
-          .text();
-        var img = $(element)
-          .find(".story-image")
-          .children("a")
-          .children("img")
-          .attr("src");
-        if (
-          headline !== "" &&
-          description !== "" &&
-          link !== "" &&
-          img !== ""
-        ) {
-          var story = {
-            headline: headline,
-            link: link,
-            description: description,
-            img: img,
-          };
-          results.push(story);
-        }
-      });
-      var oldNumber, newNumber;
-      Article.countDocuments({}, (err, count) => {
-        oldNumber = count;
-        Article.create(results)
-          .then(() => {
-            Article.countDocuments({}, (err, count) => {
-              newNumber = count;
-              res.send(`${newNumber - oldNumber} articles added!`);
-            });
-          })
-          .catch(() => {
-            Article.countDocuments({}, (err, count) => {
-              newNumber = count;
-              res.send(`${newNumber - oldNumber} articles have been added`);
-            });
-          });
-      });
-    });
-});
+
+require("./routes/html-routes")(app);
+require("./routes/api-routes")(app);
 
 app.listen(PORT, function () {
   console.log("App running on port 3000!");
